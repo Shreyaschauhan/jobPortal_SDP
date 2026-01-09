58f894585953932ed80ecc35144d554015233e12
shreyaschauhan <shreyaschauhan40@gmail.com>
2026-01-09 17:53:11 +0530

    Change getCurrentUser to GET

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -14,7 +14,7 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/changePassword").post(verifyJWT, changePassword)
-router.route("/getCurrentUser").post(verifyJWT, getCurrentUser)
+router.route("/getCurrentUser").get(verifyJWT, getCurrentUser);
 router.route("/updateAccountDetails").post(verifyJWT,upload.fields([{ name: 'resume', maxCount: 1 }, { name: 'coverImage', maxCount: 1 }]), updateAccountDetails)
 router.route("/viewProfile").post(viewProfile)
 router.route("/getalluser").post(getAllUsers)
